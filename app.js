de3d5edb9e81b2c02b0181df67bbb555df7f5189
matheustake <matheussouzamsantos@gmail.com>
2024-08-02 16:16:48 -0300

    finish

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ import express from "express";
 
 const servidor = express()
 
-servidor.get('calcular/dividir/:n1/:n2', (req, resp) => {
+servidor.get('/calcular/dividir/:n1/:n2', (req, resp) => {
 
     let n1 = Number(req.params.n1)
     let n2 = Number(req.params.n2)
 
     let div = (n1 / n2)
 
-    resp.send(`a divisão do número ${n1} com o número ${n2} é : ${div}`)
+    resp.send("a divisão é: " + div)
 })
 
 servidor.listen(5050, () => console.log('API subiu nessa porta do 5050'))
